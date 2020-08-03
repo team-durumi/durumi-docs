a6624f6923f70dcf8633c32a1ad53a9ded043e2a
woonjjang <woonjjang@gmail.com>
2020-08-03 17:23:01 +0900

    프로젝트 관련 페이지를 업데이트 합니다.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: '팀두루미 문서',
+    title: '팀두루미',
     shortName: 'durumi-docs',
     description: '팀두루미에 대한 정보를 알려드립니다.',
     imageUrl: 'https://avatars1.githubusercontent.com/u/57402612'
